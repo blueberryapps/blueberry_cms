d032b4013579eae8c58e2c86ace1d7e21d4a2299
Martin Magnusek <magnusekm@gmail.com>
2017-01-13 11:43:20 +0100

    Move view code from model to partials

--- app/models/blueberry_cms/page_blocks/gallery.rb
+++ app/models/blueberry_cms/page_blocks/gallery.rb
@@ -3,17 +3,6 @@ module BlueberryCMS
     class Gallery < PageBlock
       field :urls, type: Array, default: []
 
-      has_many :pages
-
-      def form_fields(form)
-        form.input(:urls, input_html: { value: urls.join(', ') }) +
-          form.association(:pages, collection: Page.all)
-      end
-
-      def render
-        urls
-      end
-
       def urls=(value)
         value = value.split(',').map(&:strip)
         super(value)
