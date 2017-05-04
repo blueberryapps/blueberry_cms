37d1074d66c799178da006feec2b77a01de8b48e
Tonda Pleskac <pleskac@gmail.com>
2017-05-04 15:00:23 +0200

    Fix 404 redirect

--- app/controllers/blueberry_cms/root_controller.rb
+++ app/controllers/blueberry_cms/root_controller.rb
@@ -1,7 +1,8 @@
 module BlueberryCMS
   class RootController < ApplicationController
     def index
-      redirect_to I18n.with_locale(I18n.default_locale) { Page.root.to_path }
+      page = BlueberryCMS::Page.find_by!(path: "/#{params[:path]}")
+      redirect_to I18n.with_locale(I18n.default_locale) { page.to_path }
     end
   end
 end
