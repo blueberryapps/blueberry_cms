0ef95907815a05a216168e3ba4e003f3490ccdaf
Tonda Pleskac <pleskac@gmail.com>
2017-06-13 13:13:29 +0200

    Add backgrounder for image (#19)

--- app/models/blueberry_cms/image.rb
+++ app/models/blueberry_cms/image.rb
@@ -5,6 +5,14 @@ module BlueberryCMS
     field :name, localize: true
 
     mount_uploader :image, ImageUploader
+    process_in_background :image if ::CarrierWave::Backgrounder.backend
+
+    def self.find(id)
+      _id = BSON::ObjectId.from_string(id)
+      page = BlueberryCMS::Page.find_by('blocks.images._id' => _id)
+      gallery = page.blocks.find_by('images._id' => _id)
+      gallery.images.find_by('_id' => _id)
+    end
 
     embedded_in :attachable, polymorphic: true
   end
