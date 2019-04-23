8eb276224d130c9f3e0fd9a3f4e7c1f710b40a69
Michal Gritzbach <gritzbach.michal@gmail.com>
2019-04-23 08:08:45 +0200

    Fix new page slug error

--- app/models/blueberry_cms/page.rb
+++ app/models/blueberry_cms/page.rb
@@ -46,7 +46,7 @@ module BlueberryCMS
 
     def slug_source
       renderer = Redcarpet::Markdown.new(Redcarpet::Render::StripDown)
-      renderer.render(custom_slug.presence || name).squish
+      renderer.render(custom_slug.presence || name.presence || '').squish
     end
 
     def rebuild_children_paths
--- lib/blueberry_cms/version.rb
+++ lib/blueberry_cms/version.rb
@@ -1,3 +1,3 @@
 module BlueberryCMS
-  VERSION = '0.1.1.6'.freeze
+  VERSION = '0.1.1.7'.freeze
 end
